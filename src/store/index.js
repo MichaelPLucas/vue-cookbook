b546ce82ae76e54f9b6140565cea4fb4744899d7
Michael Lucas <lucasmichael221@gmail.com>
2021-05-08 04:13:24 -0400

    Implemented first pass of Vuex store. Will need uuids and data formatting still.

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,45 @@
-export default {};
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import {
+  ADD_RECIPE,
+  DELETE_RECIPE,
+  EDIT_RECIPE,
+  EDIT_RECIPE_ADD_TAG,
+  EDIT_RECIPE_REMOVE_TAG
+} from './mutation.types';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: [],
+  getters: {
+    getAllRecipes: state => {
+      return state;
+    },
+    getRecipeById: state => id => {
+      return state.find(x => x.id == id);
+    }
+  },
+  mutations: {
+    [ADD_RECIPE] (state, recipe) {
+      state.push(recipe);
+    },
+    [DELETE_RECIPE] (state, id) {
+      state.filter(x => x.id != id);
+    },
+    [EDIT_RECIPE] (state, recipe) {
+      let index = state.findIndex(x => x.id == recipe.id);
+      if (index != -1)
+        state.splice(index, 1, recipe)
+    },
+    [EDIT_RECIPE_ADD_TAG] (state, id, tag) {
+      let index = state.findIndex(x => x.id == id);
+      state[index].tags.push(tag);
+    },
+    [EDIT_RECIPE_REMOVE_TAG] (state, id, tag) {
+      let index = state.findIndex(x => x.id == id);
+      state[index].tags = state[index].tags.filter(x => x != tag);
+    }
+  }
+});
---
+++ src/store/mutation.types.js
@@ -0,0 +1,5 @@
+export const ADD_RECIPE = 'addRecipe';
+export const DELETE_RECIPE = 'deleteRecipe';
+export const EDIT_RECIPE = 'editRecipe';
+export const EDIT_RECIPE_ADD_TAG = 'addTagToRecipe';
+export const EDIT_RECIPE_REMOVE_TAG = 'removeTagFromRecipe';
