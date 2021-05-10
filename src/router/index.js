d1da695d4184e9baf37624026b7db6597140ff8c
Michael Lucas <lucasmichael221@gmail.com>
2021-05-09 20:44:33 -0400

    Added tags to add form and added ability to create recipes.

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      name: 'home',
       path: '/',
       component: () => import('@/views/RecipeList')
     },
--- src/views/AddForm.vue
+++ src/views/AddForm.vue
@@ -4,7 +4,7 @@
       <label for="titleInput" class="form-label">
         <h5>Title</h5>
       </label>
-      <input id="titleInput" type="text" class="form-control" v-model="title">
+      <input id="titleInput" type="text" class="form-control" v-model="title" required>
     </div>
     <div class="mb-3">
       <label for="descInput" class="form-label">
@@ -28,13 +28,13 @@
         </div>
       </div>
       <div class="row">
-        <ul class="list-group">
+        <ul>
           <li v-for="ingredient in Object.keys(ingredients)"
               :key="ingredient"
               class="list-group-item">
             <div class="mb-3">
               <label for={{ingredient}} class="form-label">{{'Ingredient ' + String(parseInt(ingredient.slice(-1)) + 1)}}</label>
-              <input id={{ingredient}} type="text" class="form-control" v-model=ingredients[ingredient.slice(-1)]>
+              <input id={{ingredient}} type="text" class="form-control" v-model=ingredients[ingredient] :required="ingredient == 'ingredient0'">
             </div>
           </li>
         </ul>
@@ -50,38 +50,81 @@
         </div>
       </div>
       <div class="row">
-        <ul id="stepList">
+        <ul>
           <li v-for="step in Object.keys(steps)"
               :key="step"
               class="list-group-item">
             <div class="mb-3">
               <label for={{step}} class="form-label">{{'Step ' + String(parseInt(step.slice(-1)) + 1)}}</label>
-              <input id={{step}} type="text" class="form-control" v-model=steps[step.slice(-1)]>
+              <input id={{step}} type="text" class="form-control" v-model=steps[step] :required="step == 'step0'">
             </div>
           </li>
         </ul>
       </div>
     </div>
+    <div class="mb-3">
+      <div class="row">
+        <div class="col">
+          <h5>Tags</h5>
+        </div>
+        <div class="col text-end">
+          <button class="btn" v-on:click="addNewTag">+</button>
+        </div>
+      </div>
+      <div class="row">
+        <ul>
+          <li v-for="tag in Object.keys(tags)"
+              :key="tag"
+              class="list-group-item">
+            <div class="mb-3">
+              <input id={{tag}} type="text" class="form-control" v-model=tags[tag]>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <button class="btn btn-primary" type="submit" v-on:click="createRecipe">Add Recipe</button>
   </form>
 </template>
 
 <script>
 import Vue from 'vue';
+import store from '@/store';
+import { ADD_RECIPE } from '@/store/mutation.types';
+import router from '@/router';
 
 export default {
   name: 'AddForm',
   data: function() {
     return {
-      ingredients: {},
-      steps: {}
+      ingredients: { 'ingredient0': '' },
+      steps: { 'step0': '' },
+      tags: {}
     };
   },
   methods: {
-    addNewIngredient: function() {
+    addNewIngredient: function(event) {
+      event.preventDefault();
       Vue.set(this.ingredients, 'ingredient' + Object.keys(this.ingredients).length, '');
     },
-    addNewStep: function() {
+    addNewStep: function(event) {
+      event.preventDefault();
       Vue.set(this.steps, 'step' + Object.keys(this.steps).length, '');
+    },
+    addNewTag: function(event) {
+      event.preventDefault();
+      Vue.set(this.tags, 'tag' + Object.keys(this.tags).length, '');
+    },
+    createRecipe: function(event) {
+      store.commit(ADD_RECIPE, {
+        title: this.title,
+        description: this.description,
+        image: this.image,
+        ingredients: this.ingredients,
+        steps: this.steps,
+        tags: this.tags
+      });
+      router.push({ name: 'home' });
     }
   }
 }
